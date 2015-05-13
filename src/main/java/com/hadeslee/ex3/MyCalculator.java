c32698e3b0e1c3842ced42e412b466b5b82d53b9
hadaslee <leesk55@gmail.com>
2015-05-13 09:58:45 +0900

    Mycalculator 구현

--- src/main/java/com/hadeslee/ex3/MyCalculator.java
+++ src/main/java/com/hadeslee/ex3/MyCalculator.java
@@ -11,5 +11,32 @@ package com.hadeslee.ex3;
  */
 public class MyCalculator {
 
+    Calculator calculator;
+    private int firstNum;
+    private int secondNum;
+
+    public MyCalculator() {
+
+    }
+
+    public void add() {
+        calculator.addition(firstNum, secondNum);
+    }
+
+    public void sub() {
+        calculator.subtraction(firstNum, secondNum);
+    }
+
+    public void mul() {
+        calculator.multiplication(firstNum, secondNum);
+    }
+
+    public void div() {
+        calculator.division(firstNum, secondNum);
+    }
+
+    public void setCalculator(Calculator calculator) {
+        this.calculator = calculator;
+    }
 
 }
