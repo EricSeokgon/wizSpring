eef4459f77bd2899cf3b69dbbd773ed83499f40e
hadaslee <leesk55@gmail.com>
2015-05-13 16:03:28 +0900

    외부 파일을 이용한 설정4 개발 환경, 실행 환경 설정

---
+++ src/main/java/com/hadeslee/ex8_2/MainClass.java
@@ -0,0 +1,34 @@
+package com.hadeslee.ex8_2;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		String config = null;
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.next();
+		if(str.equals("dev")) {
+			config = "dev";
+		} else if(str.equals("run")) {
+			config = "run";
+		}
+		
+		scanner.close();
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.getEnvironment().setActiveProfiles(config);
+		ctx.load("applicationCTX_dev.xml", "applicationCTX_run.xml");
+		
+		
+		ServerInfo info = ctx.getBean("serverInfo", ServerInfo.class);
+		System.out.println("ip : " + info.getIpNum());
+		System.out.println("port : " + info.getPortNum());
+		ctx.close();
+		
+	}
+	
+}
