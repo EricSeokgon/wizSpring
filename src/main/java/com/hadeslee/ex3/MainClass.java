6fe4da6ccc3a0409775c77781fd67bec0bc6b3a9
hadaslee <leesk55@gmail.com>
2015-05-13 09:46:19 +0900

    spring without java ex init

---
+++ src/main/java/com/hadeslee/ex3/MainClass.java
@@ -0,0 +1,16 @@
+package com.hadeslee.ex3;
+
+/**
+ * Project: wizSpring
+ * FileName: MainClass
+ * Date: 2015-05-13
+ * Time: 오전 9:45
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
