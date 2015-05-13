d1f349a407201bb1f0590a182652f4478db16196
hadaslee <leesk55@gmail.com>
2015-05-13 14:05:37 +0900

    Familly spring applicationconfig @configuration @bean

---
+++ src/main/java/com/hadeslee/ex6/ApplicationConfig.java
@@ -0,0 +1,39 @@
+package com.hadeslee.ex6;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+	@Bean
+	public Student student1(){
+		
+		ArrayList<String> hobbys = new ArrayList<String>();
+		hobbys.add("수영");
+		hobbys.add("요리");
+		
+		Student student = new Student("홍길동", 20, hobbys);
+		student.setHeight(180);
+		student.setWeight(80);
+		
+		return student;
+	}
+	
+	@Bean
+	public Student student2(){
+		
+		ArrayList<String> hobbys = new ArrayList<String>();
+		hobbys.add("독서");
+		hobbys.add("음악감상");
+		
+		Student student = new Student("홍길순", 18, hobbys);
+		student.setHeight(170);
+		student.setWeight(55);
+		
+		return student;
+	}
+	
+}
